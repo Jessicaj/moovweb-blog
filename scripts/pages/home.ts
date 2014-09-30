e9e4108c254fe0c668a04ce0d5745843591e8475
Jessica Jin <samba_walk@yahoo.com>
2014-09-30 12:56:05 -0400

    moov web blog

---
+++ scripts/pages/home.ts
@@ -0,0 +1,7 @@
+
+
+$$(".meta") {
+remove("./a[@class='icon']/img")
+ 
+ move_to("..", "bottom")
+}
---
+++ scripts/sections/header.ts
@@ -0,0 +1,11 @@
+$$("header") {
+  $$("strong.logo.vcard") {
+    inner("")
+    insert("img", src: "https://s3.amazonaws.com/moovweb-apollo/logo.png")
+  }
+   $$("form.form-search") {
+    $$("input") {
+ 
+    }
+  }
+}
